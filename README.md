0eedf9975bd59a0587779acafef4a2d1eb9fbf73
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-16 20:58:01 +0200

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nextjs13-mqtt-microservices
+
+## Folder Structure
+
+```none
+nextjs13-mqtt-microservices
+└── client
+└── docs
+└── microservices
+|    ├── comments
+|    ├── moderation
+|    ├── posts
+|    └── query
+└── (...)
+```
--- microservices/posts/index.js
+++ microservices/posts/index.js
@@ -1,34 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const {randomBytes} = require('crypto');
+const { randomBytes } = require("crypto");
 const posts = {};
 
-const cors = require('cors'); 
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.get('/posts', (req,res)=>{
+app.get("/posts", (req, res) => {
   console.log("http get request received");
   res.send(posts);
 });
 
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-app.post('/posts',  async(req, res) =>{
-    const id = randomBytes(4).toString('hex');
-    const { title } = req.body;
+  posts[id] = {
+    id,
+    title,
+  };
+  console.log("http post/posts request received", posts[id]);
 
-    posts[id] = {
-        id, title 
-    };
-    console.log("http post/posts request received", posts[id]);
-    
-    // mqtt publish new posts
+  // mqtt publish new posts
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
-
-app.listen(4000, () =>{
-    console.log('Listening on port 4000')
-})
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
