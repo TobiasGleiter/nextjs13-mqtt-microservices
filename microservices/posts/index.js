09936db4516a3118ed305c89e12cc63b300160ce
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-16 21:39:48 +0200

    implemented mqtt for posts

--- microservices/posts/index.js
+++ microservices/posts/index.js
@@ -1,31 +1,48 @@
+// mqtt
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://broker.hivemq.com");
+// express
 const express = require("express");
 const app = express();
-
+// crypto
 const { randomBytes } = require("crypto");
-const posts = {};
-
+// cors
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.get("/posts", (req, res) => {
-  console.log("http get request received");
-  res.send(posts);
-});
+// ----------------------------------------------------------------
+// Constants
+const posts = {};
 
+// ----------------------------------------------------------------
+// handle http requests from client
+// post request from client
 app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
 
-  posts[id] = {
-    id,
-    title,
-  };
-  console.log("http post/posts request received", posts[id]);
+  try {
+    // create new post and hold it in the posts object
+    posts[id] = {
+      id,
+      title,
+    };
+    console.log("http post/posts request received", posts[id]);
 
-  // mqtt publish new posts
+    // mqtt publish new post
+    client.publish("/posts", JSON.stringify(posts[id]));
+    console.log("http '/posts' request published");
 
-  res.status(201).send(posts[id]);
+    // return the newly created post
+    res.status(201).send(posts[id]);
+  } catch (err) {
+    // remove the post from the posts object
+    delete posts[id];
+    console.log(err);
+
+    res.status(500).send(err);
+  }
 });
 
 app.listen(4000, () => {
--- microservices/query/index.js
+++ microservices/query/index.js
@@ -1,17 +1,46 @@
-const express = require('express');
+// mqtt
+const mqtt = require("mqtt");
+const client = mqtt.connect("mqtt://broker.hivemq.com");
+// express
+const express = require("express");
 const app = express();
-
-const cors = require('cors'); 
+// cors
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+// ----------------------------------------------------------------
+// Constants
 const posts = {};
 
-app.get('/posts',(req,res)=> {
-    console.log(posts);
-    res.send(posts);
+// ----------------------------------------------------------------
+// MQTT
+client.on("connect", () => {
+  // subscribe to the /posts topic
+  client.subscribe("/posts");
 });
 
-app.listen(4002, async() => {
-    console.log('Listening on 4002');
+client.on("message", (topic, message) => {
+  if (topic === "/posts") {
+    // Parse the message into a JavaScript object
+    const post = JSON.parse(message.toString());
+
+    // Destructure the post object
+    const { id, title } = post;
+
+    // Add the post to the posts object
+    posts[id] = { id, title, comments: [] };
+  }
+});
+
+// ----------------------------------------------------------------
+// handle http requests from client
+// post request from client
+app.get("/posts", (req, res) => {
+  console.log(posts);
+  res.status(200).send(posts);
+});
+
+app.listen(4002, async () => {
+  console.log("Listening on 4002");
 });
