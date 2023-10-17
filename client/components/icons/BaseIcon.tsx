fba4f42d0429d0a8e16ebb2946487ccd2278fef3
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-17 12:11:34 +0200

    cleanup base icons

--- client/components/icons/BaseIcon.tsx
+++ client/components/icons/BaseIcon.tsx
@@ -1,60 +1,11 @@
-import {
-  BiAdjust,
-  BiArrowBack,
-  BiBible,
-  BiCaretDown,
-  BiCaretRight,
-  BiCheckbox,
-  BiChevronRight,
-  BiDotsHorizontalRounded,
-  BiGridHorizontal,
-  BiGroup,
-  BiListPlus,
-  BiListUl,
-  BiLogIn,
-  BiLogOut,
-  BiPlus,
-  BiSearch,
-  BiTachometer,
-  BiUser,
-} from "react-icons/bi";
-import { ImArrowDown2, ImArrowUpRight2, ImSpinner9 } from "react-icons/im";
-import { SiGithub, SiGoogle, SiNextdotjs } from "react-icons/si";
+import { BiAdjust, BiArrowBack, BiCheckbox, BiPlus } from "react-icons/bi";
 
 const ICONS_MAP: any = {
-  nextjs: <SiNextdotjs />,
-  github: <SiGithub />,
-  google: <SiGoogle />,
-  spinner: <ImSpinner9 />,
-  arrowdown: <ImArrowDown2 />,
-  arrowrightup: <ImArrowUpRight2 />,
-
   plus: <BiPlus />,
-
-  caretright: <BiCaretRight />,
-  caretdown: <BiCaretDown />,
 
   arrowback: <BiArrowBack />,
 
-  dotmenu: <BiDotsHorizontalRounded />,
-
-  drag: <BiGridHorizontal />,
-
-  search: <BiSearch />,
-
-  chevronright: <BiChevronRight />,
-
   logo: <BiAdjust />,
-  dashboard: <BiTachometer />,
-  equipment: <BiListUl />,
-  newequipment: <BiListPlus />,
-  sundayservice: <BiBible />,
-  users: <BiGroup />,
-
-  profile: <BiUser />,
-
-  signin: <BiLogIn />,
-  signout: <BiLogOut />,
 
   placeholder: <BiCheckbox />,
 };
